d933298365026fea78fc05b11e3b7980338cd163
Caio <caioqs.info@gmail.com>
2022-05-18 20:46:17 -0300

    Instalado dependências do projeto e começando a fazer o login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo-devs.png'
+
+
+const Login = () => {
+  return(
+    <form>
+      <img src={logo} alt="Logo devs family" />
+    </form>
+  );
+};
+
+export default Login;
