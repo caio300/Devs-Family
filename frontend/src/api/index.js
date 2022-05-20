4d5359b23010d14b7c792f7a5cd79778a0528225
Caio <caioqs.info@gmail.com>
2022-05-20 17:30:02 -0300

    Criando pagina login: erro ao conectar com API

---
+++ frontend/src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001'
+});
+
+api.interceptors.request.use(async config => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  if (token) {
+    config.headers.authorization = `${token}`
+  }
+  return config;
+});
+
+export default api;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
-import logo from './logo-devs.png'
+import React, { useState } from 'react';
+import logo from '../logo-devs.png';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import "../style/Login.css";
 
 
 const Login = () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const tokenAPI = await axios.get('http://localhost:3001/devs');
+    console.log(tokenAPI);
+  };
+
   return(
-    <form>
-      <img src={logo} alt="Logo devs family" />
-    </form>
+    <div className='container'>
+      <form className='container-form' onSubmit={(event) => handleSubmit(event)}>
+        <img className='container-img' src={logo} alt="Logo devs family" />
+        <label className='label-inputs' htmlFor="email">
+          Email
+          <input value={email} onChange={({target}) => setEmail(target.value)} type="email" />
+        </label>
+        <label className='label-inputs' htmlFor="password">
+          Senha
+          <input value={password} onChange={({target}) => setPassword(target.value)} type="password" />
+        </label>
+        <button disabled={(!email || !password)} className='buttom-login' type='submit'>Entrar</button>
+      </form>
+    </div>
   );
 };
 
 export default Login;
+
+
+/* if (tokenAPI.data.token) {
+  localStorage.setItem('token', tokenAPI.data.token)
+  history.push('/home');
+}  */
